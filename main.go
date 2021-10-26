460daa5e10e9e7d46402636fb698ff19f1721950
Hyungyu Park <Hyungyu.Park@gordon.edu>
2021-10-25 22:38:14 -0400

    combining image and audio to make a video

--- main.go
+++ main.go
@@ -7,21 +7,28 @@ import (
 )
 
 //location of your repository
-var basePath = "C:/Users/sehee/OneDrive - Gordon College/Desktop/Gordon/Senior/Senior Project/SIL-Video"
+//var basePath = "C:/Users/sehee/OneDrive - Gordon College/Desktop/Gordon/Senior/Senior Project/SIL-Video"	//windows
+var basePath = "/Users/hyungyu/Documents/SIL-Video"	//mac
 
 //image name
 var imageName = "VB-John 4v43-44.jpg"
+
+//audio name
+var audioName = "inputs_mp3_44-JHNgul-01.mp3"
 
 //video name
 var videoName = "video.mp4"
 
 //location of where you downloaded FFmpeg
-var baseFFmpegPath = "C:/FFmpeg"
+//var baseFFmpegPath = "C:/FFmpeg"	//windows
+var baseFFmpegPath = "/usr/local/"	//mac
 
-var FfmpegBinPath = baseFFmpegPath + "/bin/ffmpeg"
-var FfprobeBinPath = baseFFmpegPath + "/bin/ffprobe"
+var FfmpegBinPath = baseFFmpegPath + "/bin/ffmpeg"	//both windows, mac
+var FfprobeBinPath = baseFFmpegPath + "/bin/ffprobe"	//both windows, mac
 
-var inputPath = basePath + "/input/image/" + imageName
+//var inputPath = basePath + "/input/"
+var inputImagePath = basePath + "/input/image/" + imageName
+var inputAudioPath = basePath + "/input/audio/" + audioName
 var outputPath = basePath + "/output/" + videoName
 
 func main() {
@@ -42,8 +49,9 @@ func main() {
 
 	progress, err := ffmpeg.
 		New(ffmpegConf).
-		Input(inputPath).
-		Output(outputPath).
+		//Input(inputPath).
+		Input(inputImagePath).
+		Output(tempPath).
 		WithOptions(opts).
 		Start(opts)
 
