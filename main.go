fcb0ec69dc1a781f62a0f702ad0eb5e3a4216373
Hyungyu Park <Hyungyu.Park@gordon.edu>
2021-12-05 16:45:22 -0500

    commit David's suggestion. Tested and worked

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
-	"os"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"strings"
 	//"os/exec"
 )
 
 // File Location of Repository **CHANGE THIS FILEPATH TO YOUR REPOSITORY FILEPATH**
-var basePath = "/Users/gordon.loaner/OneDrive - Gordon College/Desktop/Gordon/Senior/Senior Project/SIL-Video" //sehee
-// var basePath = "/Users/hyungyu/Documents/SIL-Video"	//hyungyu
+// var basePath = "/Users/gordon.loaner/OneDrive - Gordon College/Desktop/Gordon/Senior/Senior Project/SIL-Video" //sehee
+var basePath = "/Users/hyungyu/Documents/SIL-Video" //hyungyu
 //var basePath = "C:/Users/damar/Documents/GitHub/SIL-Video" // david
 // var basePath = "/Users/roddy/Desktop/SeniorProject/SIL-Video/"
 
 //location of where you downloaded FFmpeg
-var baseFFmpegPath = "C:/FFmpeg" //windows
-// var baseFFmpegPath = "/usr/local/"	//mac
+// var baseFFmpegPath = "C:/FFmpeg" //windows
+var baseFFmpegPath = "/usr/local/" //mac
 
 var FfmpegBinPath = baseFFmpegPath + "/bin/ffmpeg"
 var FfprobeBinPath = baseFFmpegPath + "/bin/ffprobe"
@@ -91,22 +91,22 @@ func createTempVideos(paths ...string) {
 
 func findVideos() {
 	textfile, err := os.Create(basePath + "/output/text.txt")
-    check(err)
+	check(err)
 
-    defer textfile.Close()
+	defer textfile.Close()
 
-	files, err := ioutil.ReadDir(basePath+"/output")
-    if err != nil {
-        log.Fatal(err)
-    }
+	files, err := ioutil.ReadDir(basePath + "/output")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, file := range files {
-		if(strings.Contains(file.Name(), ".mp4")) {
+	for _, file := range files {
+		if strings.Contains(file.Name(), ".mp4") {
 			textfile.WriteString("file ")
 			textfile.WriteString(file.Name())
 			textfile.WriteString("\n")
 		}
-    }
+	}
 
 	textfile.Sync()
 }
@@ -122,29 +122,5 @@ func combineVideos() {
 	)
 
 	err := cmd.Run() // Start a process on another goroutine
-	check(err)
-}
-
-func convertToVideo(paths ...string) {
-	// Here we can parse an individual element from paths
-	fmt.Println(paths)
-	// Here we can iterate through each element and access it
-	for index, value := range paths {
-		fmt.Println(index)
-		fmt.Println(value)
-	}
-
-	cmd := exec.Command("ffmpeg",
-		"-framerate", "1", // frame  to define how fast the pictures are read in, in this case, 1 picture per second
-		"-i", basePath+"/image-%d.jpg", // input image
-		"-r", "30", // the framerate of the output video
-		"-i", basePath+"/narration-001.mp3", // input audio
-		basePath+"/output/output.mp4", // output
-	)
-
-	err := cmd.Start() // Start a process on another goroutine
-	check(err)
-
-	err = cmd.Wait() // wait until ffmpeg finishg
 	check(err)
 }
