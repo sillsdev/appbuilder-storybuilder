cba78f92f56e1f044677c9a5b46c7fec8008133f
Davideo37 <damargur@gmail.com>
2021-12-08 22:35:23 -0500

    Tentative dynamic code

--- main.go
+++ main.go
@@ -18,38 +18,59 @@ var basePath = "C:/Users/damar/Documents/GitHub/SIL-Video" // david
 
 func main() {
 	// First we parse in the various pieces from the template
-	var outputPath = "./output"
+	Images := []string{};
+	Audios := []string{};
+	//BackAudioPath := "";
+	//BackAudioVolume := "";
+	Transitions := []string{};
+	TransitionDurations := []string{};
+	Timings := [][]string{};
+
+	//var outputPath = "./output"
 	fmt.Println("Parsing .slideshow file...")
-	var slideshow = readData()
-	var titleimg = slideshow.Slide[0].Image.Name
-	var img1 = slideshow.Slide[1].Image.Name
-	var img2 = slideshow.Slide[2].Image.Name
-	var img3 = slideshow.Slide[3].Image.Name
-	var introAudio = slideshow.Slide[0].Audio.Background_Filename.Path
-	var introVolume = slideshow.Slide[0].Audio.Background_Filename.Volume
-	var audio1 = slideshow.Slide[1].Audio.Filename.Name
-	var title_start = slideshow.Slide[0].Timing.Start
-	var title_duration = slideshow.Slide[0].Timing.Duration
-	var img1_start = slideshow.Slide[1].Timing.Start
-	var img1_duration = slideshow.Slide[1].Timing.Duration
-	var img2_start = slideshow.Slide[2].Timing.Start
-	var img2_duration = slideshow.Slide[2].Timing.Duration
-	var img3_start = slideshow.Slide[3].Timing.Start
-	var img3_duration = slideshow.Slide[3].Timing.Duration
+	var slideshow = readData();
+	for i, slide := range slideshow.Slide {
+		if (i == 0){
+			//BackAudioPath = slide.Audio.Background_Filename.Path;
+			//BackAudioVolume = slide.Audio.Background_Filename.Volume;
+		} else {
+			Audios = append(Audios, slide.Audio.Filename.Name);
+		}
+		Images = append(Images, slide.Image.Name);
+		Transitions = append(Transitions, slide.Transition.Type);
+		TransitionDurations = append(TransitionDurations, slide.Transition.Duration);
+		temp := []string{slide.Timing.Start, slide.Timing.Duration};
+		Timings = append(Timings, temp);
+		fmt.Println(Timings[0][0]);
+	}
+	// var titleimg = slideshow.Slide[0].Image.Name
+	// var img1 = slideshow.Slide[1].Image.Name
+	// var img2 = slideshow.Slide[2].Image.Name
+	// var img3 = slideshow.Slide[3].Image.Name
+	// var introAudio = slideshow.Slide[0].Audio.Background_Filename.Path
+	// var audio1 = slideshow.Slide[1].Audio.Filename.Name
+	// var title_start = slideshow.Slide[0].Timing.Start
+	// var title_duration = slideshow.Slide[0].Timing.Duration
+	// var img1_start = slideshow.Slide[1].Timing.Start
+	// var img1_duration = slideshow.Slide[1].Timing.Duration
+	// var img2_start = slideshow.Slide[2].Timing.Start
+	// var img2_duration = slideshow.Slide[2].Timing.Duration
+	// var img3_start = slideshow.Slide[3].Timing.Start
+	// var img3_duration = slideshow.Slide[3].Timing.Duration
 
 	// Place them all inside a string slice
-	paths := []string{outputPath, titleimg, img1, img2, img3, introAudio, audio1, title_start, title_duration, img1_start, img1_duration, img2_start, img2_duration, img3_start, img3_duration}
-	fmt.Println("Finished parsing .slideshow...")
-	fmt.Println("Creating temporary videos...")
-	createTempVideos(paths...)
-	fmt.Println("Finished creating temporary videos...")
-	fmt.Println("Fetching temporary video paths...")
-	findVideos()
-	fmt.Println("Finished fetching temporary video paths...")
-	fmt.Println("Combining temporary videos into single video...")
-	combineVideos()
-	fmt.Println("Finished combining temporary videos...")
-	addBackgroundMusic(introAudio, introVolume)
+	//paths := []string{outputPath, titleimg, img1, img2, img3, introAudio, audio1, title_start, title_duration, img1_start, img1_duration, img2_start, img2_duration, img3_start, img3_duration}
+	// fmt.Println("Finished parsing .slideshow...")
+	// fmt.Println("Creating temporary videos...")
+	createTempVideos(Images, Audios, Transitions, TransitionDurations, Timings);
+	// fmt.Println("Finished creating temporary videos...")
+	// fmt.Println("Fetching temporary video paths...")
+	// findVideos()
+	// fmt.Println("Finished fetching temporary video paths...")
+	// fmt.Println("Combining temporary videos into single video...")
+	// combineVideos()
+	// fmt.Println("Finished combining temporary videos...")
+	// addBackgroundMusic(BackAudioPath, BackAudioVolume)
 }
 
 func check(err error) {
@@ -59,21 +80,45 @@ func check(err error) {
 	}
 }
 
-func createTempVideos(paths ...string) {
-	fmt.Println(paths)
-	for i := 1; i <= 3; i++ {
+func createTempVideos(Images []string, Audios []string, Transitions []string, TransitionDurations []string, Timings [][]string) {
+	cmd := exec.Command("")
+	for i := 0; i < len(Images); i++ {
 		fmt.Println("Creating video", i)
-		cmd := exec.Command("ffmpeg",
-			// "-i", fmt.Sprintf("%s/input/image-%d.jpg", basePath, i), // input image
-			"-i", basePath+"/input/"+paths[i + 1],
-			"-r", "30", // the framerate of the output video
-			"-ss", paths[9+2*i-2]+"ms",
-			"-t", paths[10+2*i-2]+"ms",
-			"-i", basePath+"/input/narration-001.mp3", // input audio
-			"-pix_fmt", "yuv420p",
-			"-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2",
-			fmt.Sprintf("%s/output/output%d.mp4", basePath, i), // output
+		// The credits slide has no timings specified, so calculate a start from the previous slides numbers
+		if (Images[i] == Images[len(Images)-1]){
+			prevSlideStart := int64(0)
+			prevSlideDuration := int64(0)
+			if s, err := strconv.ParseInt(Timings[i-1][0], 10, 0); err == nil {
+				prevSlideStart = s;
+			} else if e, err := strconv.ParseInt(Timings[i-1][1], 10, 0); err == nil{
+				prevSlideDuration = e;
+			} else {
+				fmt.Println("Error converting slide timings to int")
+			}
+			creditsStart := prevSlideStart + prevSlideDuration
+			cmd = exec.Command("ffmpeg",
+				"-i", basePath+"/input/"+Images[i],
+				"-r", "30", // the framerate of the output video
+				"-ss", string(creditsStart)+"ms",
+				"-i", basePath+"/input/narration-001.mp3", // input audio
+				"-pix_fmt", "yuv420p", // Formatting options
+				"-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2",
+				fmt.Sprintf("%s/output/output%d.mp4", basePath, i), // output
 		)
+		} else {
+			cmd = exec.Command("ffmpeg",
+				// "-i", fmt.Sprintf("%s/input/image-%d.jpg", basePath, i), // input image
+				"-i", basePath+"/input/"+Images[i],
+				"-r", "30", // the framerate of the output video
+				"-ss", Timings[i][0]+"ms",
+				"-t", Timings[i][1]+"ms",
+				"-i", basePath+"/input/narration-001.mp3", // input audio
+				"-pix_fmt", "yuv420p", // Formatting options
+				"-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2",
+				fmt.Sprintf("%s/output/output%d.mp4", basePath, i), // output
+		)
+	}	
+		fmt.Println(cmd)
 		err := cmd.Start() // Start a process on another goroutine
 		check(err)
 		fmt.Println("Command started")
@@ -118,7 +163,7 @@ func combineVideos() {
 
 func addBackgroundMusic(backgroundAudio string, backgroundVolume string) {
 	// Convert the background volume to a number between 0 and 1
-	var tempVol = 0.0
+	tempVol := 0.0
 	if s, err := strconv.ParseFloat(backgroundVolume, 32); err == nil {
         tempVol = s;
     } else {
