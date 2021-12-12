9163e2e5468bf3d97d9198f9ffcfe4befb05605c
Gordon Loaner <gordon.loaner@gc06251.gordon.edu>
2021-12-12 17:30:22 -0500

    Combines everything except title and credits page

--- main.go
+++ main.go
@@ -58,47 +58,23 @@ func checkCMDError(output []byte, err error) {
 }
 
 func combineVideos(Images []string, Transitions []string, TransitionDurations []string, Timings [][]string, Audios []string) {
-	fmt.Println(Timings, Images)
+	fmt.Println(TransitionDurations)
 	input_images := []string{}
 	input_filters := ""
-	//4
 	totalNumImages := len(Images) - 1
 	concatTransitions := ""
-
-	// input_images := []string{}
-	// input_filters := ""
-	// totalNumImages := 3
-	// concatTransitions := ""
-
-	// fmt.Println("Getting list of images and filters...")
-	// for i := 1; i <= totalNumImages; i++ {
-	// 	input_images = append(input_images, "-loop", "1", "-ss", paths[9+2*i-2]+"ms", "-t", paths[10+2*i-2]+"ms", "-i", basePath+"/input/"+paths[i+1])
-	// 	concatTransitions += fmt.Sprintf("[v%d]", i-1)
-	// 	if i == 1 {
-	// 		input_filters += fmt.Sprintf("[0:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=out:st=%s:d=1000ms[v0];", paths[10])
-	// 	} else {
-	// 		input_filters += fmt.Sprintf("[%d:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=in:st=0:d=1000ms,fade=t=out:st=%sms:d=1000ms[v%d];", i-1, paths[10+2*i-2], i-1)
-	// 	}
-	// }
-
-	// concatTransitions += fmt.Sprintf("concat=n=%d:v=1:a=0,format=yuv420p[v]", totalNumImages)
-	// input_filters += concatTransitions
-
-	// input_images = append(input_images, "-i", basePath+"/input/narration-001.mp3", "-filter_complex", input_filters, "-map", "[v]",
-	// 	"-map", fmt.Sprintf("%d:a", totalNumImages),
-	// 	"-shortest", basePath+"/output/mergedVideo.mp4")
-
-	// fmt.Println("Creating video...")
-	// cmd := exec.Command("ffmpeg", input_images...)
 
 	fmt.Println("Getting list of images and filters...")
 	for i := 1; i < totalNumImages-1; i++ {
 		input_images = append(input_images, "-loop", "1", "-ss", Timings[i][0]+"ms", "-t", Timings[i][1]+"ms", "-i", basePath+"/input/"+Images[i])
-		concatTransitions += fmt.Sprintf("[v%d]", i)
+		concatTransitions += fmt.Sprintf("[v%d]", i-1)
 		if i == 1 {
-			input_filters += "[1:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=out:st=1000ms:d=1000ms[v1];"
+			input_filters += fmt.Sprintf("[0:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=out:st=%sms:d=%sms[v0];", Timings[i][1], TransitionDurations[i])
 		} else {
-			input_filters += fmt.Sprintf("[%d:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=in:st=0:d=1000ms,fade=t=out:st=%sms:d=1000ms[v%d];", i, Timings[i][1], i)
+			half_duration, err := strconv.Atoi(TransitionDurations[i])
+			check(err)
+
+			input_filters += fmt.Sprintf("[%d:v]crop=trunc(iw/2)*2:trunc(ih/2)*2,fade=t=in:st=0:d=%dms,fade=t=out:st=%sms:d=%dms[v%d];", i-1, half_duration/2, Timings[i][1], half_duration/2, i-1)
 		}
 	}
 
