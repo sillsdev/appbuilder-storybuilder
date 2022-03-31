14c5665839e63f9ea6a6bff8aad84495779ce9a2
SeHee Hyung <sehee.hyung@gordon.edu>
2022-03-31 13:25:57 +0100

    add test for getting video length

--- TemplateVideo/main_test.go
+++ TemplateVideo/main_test.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strconv"
 	"strings"
 	"testing"
 )
+
+var ffmpeg string
+
+func init() {
+	cmd := exec.Command("where", "ffmpeg")
+	output, err := cmd.CombinedOutput()
+	checkCMDError(output, err)
+
+	ffmpeg = strings.TrimSpace(string(output))
+}
 
 func TestParse(t *testing.T) {
 	templateName := "../TestInput/test.slideshow"
@@ -134,6 +145,38 @@ func Test_scaleImages(t *testing.T) {
 	}
 }
 
+func Test_cmdCreateTempVideo(t *testing.T) {
+	type args struct {
+		ImageDirectory       string
+		duration             string
+		zoom_cmd             string
+		finalOutputDirectory string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *exec.Cmd
+	}{
+		{
+			"Creating temp video ffmpeg command for VB-John 1v1.jpg",
+			args{ImageDirectory: "../TestInput/VB-John 1v1.jpg",
+				duration:             "9400",
+				zoom_cmd:             "scale=8000:-1,zoompan=z='1/((0.718)-(0.001)*on)':x='0.282*iw+0.000*iw*on':y='0.088*ih+0.000*ih*on':d=235:fps=25,scale=1280:720,setsar=1:1",
+				finalOutputDirectory: "./temp/temp1-8.mp4"},
+			exec.Command(ffmpeg+" -loop", "1", "-i", "./../TestInput/VB-John 1v1.jpg", "-t",
+				"9400ms", "-filter_complex", "scale=8000:-1,zoompan=z='1/((0.718)-(0.001)*on)':x='0.282*iw+0.000*iw*on':y='0.088*ih+0.000*ih*on':d=235:fps=25,scale=1280:720,setsar=1:1", "-shortest", "-pix_fmt", "yuv420p", "-y", "./temp/temp1-8.mp4"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println(cmdCreateTempVideo(tt.args.ImageDirectory, tt.args.duration, tt.args.zoom_cmd, tt.args.finalOutputDirectory).String())
+			if got := cmdCreateTempVideo(tt.args.ImageDirectory, tt.args.duration, tt.args.zoom_cmd, tt.args.finalOutputDirectory).String(); got != tt.want.String() {
+				t.Errorf("cmdCreateTempVideo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_createZoomCommand(t *testing.T) {
 	type args struct {
 		Motions  [][]float64
@@ -155,6 +198,37 @@ func Test_createZoomCommand(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := createZoomCommand(tt.args.Motions, tt.args.Duration); got != tt.want {
 				t.Errorf("createZoomCommand() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cmdGetVideoLength(t *testing.T) {
+	type args struct {
+		inputDirectory string
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			"get correct video duration",
+			args{inputDirectory: "../TestInput/sample_video.mp4"},
+			45.0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := cmdGetVideoLength(tt.args.inputDirectory)
+			output, err := cmd.CombinedOutput()
+			checkCMDError(output, err)
+
+			video_length, err := strconv.ParseFloat(strings.TrimSpace(string(output)), 8)
+
+			if video_length != tt.want {
+				t.Errorf("expected video length of sample_video.mp4 to be %f but got %f", tt.want, video_length)
 			}
 		})
 	}
